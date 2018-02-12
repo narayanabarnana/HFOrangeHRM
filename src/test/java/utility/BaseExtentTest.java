d21392bba1e401429b7273c2146a2b2ef6d2cbfe
Narayana <Narayana@DESKTOP-E8IIGOH>
2018-02-12 10:31:12 +0530

    Commited
    Signed-off-by: Narayana <Narayana@DESKTOP-E8IIGOH>

---
+++ src/test/java/utility/BaseExtentTest.java
@@ -0,0 +1,73 @@
+package utility;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class BaseExtentTest {
+	
+	 WebDriver driver;
+	public static ExtentReports extent;
+	public static ExtentTest logger;
+	public static ExtentHtmlReporter htmlReporter;
+	
+	@BeforeSuite
+	public void setUp()
+	{
+		htmlReporter = new ExtentHtmlReporter("./Reports/LoginPage.html");
+		extent = new ExtentReports();
+		extent.config();
+		extent.attachReporter(htmlReporter);
+		
+		htmlReporter.config().setReportName("Report");
+		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+		htmlReporter.config().setTheme(Theme.STANDARD);
+	}
+	
+	
+	@AfterMethod
+	public void getResult(ITestResult result) throws Exception{
+		
+		if(result.getStatus()==ITestResult.FAILURE)
+		{
+			String screenshotPath=GetScreenshot.capture(driver, result.getName());
+			//logger.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + "test case failed due to below issues", ExtentColor.RED));
+			logger.fail(MarkupHelper.createLabel(result.getName() + "test case failed due to below issues", ExtentColor.RED));
+			logger.fail(result.getThrowable());
+			logger.fail("Screenshot below" + logger.addScreenCaptureFromPath(screenshotPath));
+		}
+		
+		else if(result.getStatus()==ITestResult.SUCCESS)
+		{
+			logger.pass(MarkupHelper.createLabel(result.getName() + "test case Passed", ExtentColor.GREEN));
+		}
+		else
+		{
+			//logger.skip(Status.SKIP, MarkupHelper.createLabel(result.getName() + "test case Passed", ExtentColor.YELLOW));
+			logger.skip(MarkupHelper.createLabel(result.getName() + "test case Skipped", ExtentColor.YELLOW));
+			logger.skip(result.getThrowable());
+		}
+		extent.flush();
+		
+	}
+	
+	
+	@AfterSuite
+	public void teardown()
+	{
+		extent.flush();
+	}
+	
+
+}
---
+++ src/test/java/utility/GetScreenshot.java
@@ -0,0 +1,26 @@
+package utility;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class GetScreenshot {
+	
+	public static String capture(WebDriver driver, String screenshotName) throws Exception
+	{
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File source=ts.getScreenshotAs(OutputType.FILE);
+		
+		String dest = "F:\\Selenium Framework\\com.orangeHRM.hybridFramework\\Screenshots"+screenshotName+".png";
+		File destination = new File(dest);
+		FileUtils.copyFile(source, destination);
+		
+		return dest;
+		
+	}
+
+}
